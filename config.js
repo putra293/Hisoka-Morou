ab9fbb266a9dfab1570fcfe1b48abe1f3f98e82c
putra293 <102336391+putra293@users.noreply.github.com>
2022-06-14 21:36:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,11 +18,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
+global.owner = ['6283840600765','6283840600765','6283840600765']
+global.premium = ['6283856051043']
+global.packname = 'PUTRA PLER'
 global.author = 'WhatsApp Bot'
-global.sessionName = 'hisoka'
+global.sessionName = 'putra'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
